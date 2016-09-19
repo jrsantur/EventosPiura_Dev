453b0c88b73924210aa4f8c226b27dfecbc06227
Junior <jrsantur06@gmail.com>
2016-09-19 18:52:46 -0500

    Implementando  Rest

---
+++ model/src/main/java/com/model/events/entities/Usuario.java
@@ -0,0 +1,8 @@
+package com.model.events.entities;
+
+/**
+ * Created by Junior on 19/09/2016.
+ */
+
+public class Usuario {
+}
---
+++ model/src/main/java/com/model/events/rest/wrappers/Grupo.java
@@ -0,0 +1,16 @@
+package com.model.events.rest.wrappers;
+
+import com.model.events.entities.Usuario;
+
+import java.util.List;
+
+/**
+ * Created by Junior on 19/09/2016.
+ */
+
+public class Grupo {
+    int id;
+    List<Usuario>  users;
+    int cantidad ;
+
+}
